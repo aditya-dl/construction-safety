a6c76ba85d3d6645b1f30e15cd3600bcf027b943
Aditya Lohia <adityaslohia29@gmail.com>
2021-02-03 23:11:00 +0530

    add: working code for detection

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -1,4 +1,5 @@
 # import dependencies
+import numpy as np 
 import cv2
 import torch
 
@@ -13,11 +14,14 @@ def inference(model, image, size=640):
     return model(image, size=size)
 
 if __name__ == "__main__":
-    image_path = "construction-safety/images/test1.jpg"
-    model_path = 'construction-safety/yolov5/weights/last.pt'
+    image_path = "../images/2_348.png"
+    model_path = './weights/last.pt'
+    labels = ['JCB', 'Person', 'Truck', 'Helmet', 'Crane', 'Jacket']
 
     model = load_model(model_path)
     image = read_image(image_path)
 
     results = inference(model, image)
-    print(results)
+    dets = results.xyxy[0]
+    dets = [list(map(int, lst)) for lst in dets]
+    print(dets)
